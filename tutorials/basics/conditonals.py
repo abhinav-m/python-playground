bf11cc32ca4b05829f4f8e00a41f5ed8c93b19a8
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-16 23:46:04 +0530

    Added one example

--- tutorials/basics/conditonals.py
+++ tutorials/basics/conditonals.py
@@ -6,3 +6,21 @@ elif name == 'Arya Stark':
     print("Valar Morghulis")
 else:
     print("Carry on")
+
+
+#Truthy and falsy values in python
+
+if 0:
+    print("Falsy.")
+# 1 is truthy
+if 1:
+    print("Truthy")
+
+
+#Empty strings are falsy in python.
+
+test = input()
+if test:
+    print("You said "+ test)
+else:
+    print("SAY SOMETHING!!")
