89f54333bee1faa8b8a2b0a7b1d5d3e5998cfccc
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-17 23:57:55 +0530

    minor changes

---
+++ tutorials/basics/logicalOperators.py
@@ -0,0 +1,9 @@
+age = 5
+
+# and keyword for and operator
+if age > 3 and age < 6:
+    print("Age between 3 and 6")
+
+# or keyword for or operator
+if age > 6 or age < 7:
+    print("Age is less than 7 or greater than 6")
