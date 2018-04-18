8642a24b0a92024205966c425a42f7fa0b5e6a54
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-18 22:48:26 +0530

    Added example on checking equality and not operator

---
+++ tutorials/basics/checking_equality.py
@@ -0,0 +1,45 @@
+# To check equality, == operator can be used.
+x = 13
+y = 13
+
+print(x == y)
+
+# To check reference of variable in memory, is operator can be used.
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+# Will print false as both lists have different references.
+print(a is b)
+
+clone = a
+
+# prints true
+print(a is clone)
+
+age = input("How old are you")
+
+""" # 18-21 wristband
+if age:
+    age = int(age)
+    if (age >= 18 and age < 21):
+        print("wristband")
+    # 21+ drink, normal entry
+    elif(age >= 21):
+        print("Normal entry")
+    else:
+        print("You're too young, sorry")
+else:
+    print("Please enter age") """
+
+
+if age:
+    age = int(age)
+    if age >= 21:
+        print("You can enter and drink")
+    elif age >= 18:
+        print("You can enter, need a wristband")
+    else:
+        print("You cant  come in, little one")
+else:
+    print("Invalid age")
--- tutorials/basics/logicalOperators.py
+++ tutorials/basics/logicalOperators.py
@@ -7,3 +7,9 @@ if age > 3 and age < 6:
 # or keyword for or operator
 if age > 6 or age < 7:
     print("Age is less than 7 or greater than 6")
+
+# not keyword for not operator
+if not age >= 18:
+    print("You are not an adult")
+else:
+    print("You are an adult")
