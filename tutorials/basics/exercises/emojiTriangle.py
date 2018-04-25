5c8818bdf6955e598c72b97083d9020e96bb6d55
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-25 22:40:10 +0530

    Added loops examples

--- tutorials/basics/exercises/emojiTriangle.py
+++ tutorials/basics/exercises/emojiTriangle.py
@@ -1,5 +1,18 @@
 emoji = "\U0001f600"
 
 
-for x in range(1,11):
+for x in range(1, 11):
     print(emoji * x)
+
+times = 1
+while times < 11:
+    print(emoji * times)
+    times += 1
+
+for num in range(1, 11):
+    count = 1
+    smileys = ''
+    while count <= num:
+        smileys += emoji
+        count += 1
+    print(smileys)
