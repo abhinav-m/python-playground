add0c2e0a04dc2324b27e0314d2feb45b0590315
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-25 23:37:33 +0530

    Added solution to mini exercise

---
+++ tutorials/basics/mini-projects/guessing_game.py
@@ -0,0 +1,22 @@
+import random
+
+random_number = random.randint(1, 10)
+
+
+play = True
+
+while play:
+    print("Guess the number")
+    number = int(input())
+    while number != random_number:
+        number = int(input())
+        if number < random_number:
+            print("Too low")
+        elif number > random_number:
+            print("Too high")
+    print("You guessed correct")
+    play = input("Do you want to play again?")
+    if play == 'y' or play == 'Y':
+        play = True
+    else:
+        play = False
