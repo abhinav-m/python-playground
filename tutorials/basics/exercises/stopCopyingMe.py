c55ece4a642994df4c0c0365b3ed72eada17c067
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-25 22:51:16 +0530

    Added exercise on loop

---
+++ tutorials/basics/exercises/stopCopyingMe.py
@@ -0,0 +1,10 @@
+print("Hey how it's going?")
+
+answer = ""
+
+WINNING_ANSWER = "UGH FINE YOU WIN"
+
+while answer != WINNING_ANSWER:
+    answer = input()
+    if(answer != WINNING_ANSWER):
+        print(answer)
