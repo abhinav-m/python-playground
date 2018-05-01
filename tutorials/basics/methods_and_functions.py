d71133855fd72e9d31274938ba12154e1ab21b69
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-01 22:52:51 +0530

    Added examples of methods and functions in lists

---
+++ tutorials/basics/methods_and_functions.py
@@ -0,0 +1,17 @@
+# functions 
+len([1,2,3])
+
+# method
+# this method is on the 'list' object
+data = [1,2,3]
+data.append(4)
+print(data)
+
+# extend takes the value in the list and adds them to the end of the given array
+data.extend([5,6,7])
+print(data)
+
+# append takes the value in the list and adds them to the end of the given array as a single element
+nums = [1,2,3]
+nums.append([4,5])
+print(nums)
