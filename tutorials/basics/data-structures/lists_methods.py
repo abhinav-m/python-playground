51a52b7265f8fd244dd3e3ce23688d7071880925
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-06 21:27:18 +0530

    Added examples of list methods

--- tutorials/basics/data-structures/lists_methods.py
+++ tutorials/basics/data-structures/lists_methods.py
@@ -17,3 +17,34 @@ names = ["Abhinav", "Athar", "Abhinav", "Abhinav", "Raju", "Athar", "Raju"]
 
 # start is inclusive , end is not -> will throw error on 3,4
 print(names.index("Raju", 3, 5))
+
+# prints count of a value in a list.
+cnt = names.count("Raju")
+print(cnt)
+
+
+# If value is not present, returns 0 , unlike index which throws error.
+print(names.count("akash"))
+# Logs 0
+
+nums = [1, 2, 3, 4, 5]
+
+# Reverses list in place
+nums.reverse()
+
+print(nums)
+
+
+# sorts list in place
+nums = [5, 2, 1, 3, 6]
+
+nums.sort()
+
+print(nums)
+
+
+# join method -> string method which takes iterable argument
+# and joins them using the given delimiter.
+
+words = ["I", "am", "learning", "python"]
+print("#".join(words))
