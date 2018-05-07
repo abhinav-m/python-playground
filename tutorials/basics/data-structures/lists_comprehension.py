df80c1eeecd0f7c0214801332d49c8d1caff2b44
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-07 22:27:45 +0530

    Added more examples for list comprehension

--- tutorials/basics/data-structures/lists_comprehension.py
+++ tutorials/basics/data-structures/lists_comprehension.py
@@ -23,3 +23,13 @@ print([name.capitalize() for name in names])
 
 # Using string slicing.
 print([name[0].upper() + name[1::] for name in names])
+
+# Using with ranges.
+print([num - 1 for num in range(1, 11)])
+
+
+# Checking falsy values by wrapping in bool.
+print([bool(val) for val in ["", {}, [], 0, -1, 1]])
+
+# Conversion of values to string or any other type.
+print([str(num) for num in [1.0, 2.5, 3.14, 9.62]])
