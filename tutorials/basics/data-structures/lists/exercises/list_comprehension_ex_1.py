9f3d82d5d9f20bd3c4fe401e0f167531dc1c6543
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-07 22:51:12 +0530

    added exercise solutions

--- tutorials/basics/data-structures/lists/exercises/list_comprehension_ex_1.py
+++ tutorials/basics/data-structures/lists/exercises/list_comprehension_ex_1.py
@@ -1,5 +1,14 @@
 # First letter of a list
-answer = [name[0] for name in ["Ellie", "Tim", "Matt"]]
+first_letters = [name[0] for name in ["Ellie", "Tim", "Matt"]]
 
 # Even numbers in a given list
-answer2 = [num for num in [1, 2, 3, 4, 5, 6] if num % 2 == 0]
+even_numbers = [num for num in [1, 2, 3, 4, 5, 6] if num % 2 == 0]
+
+# Intersection of two lists
+list_1 = [1, 2, 3, 4]
+list_2 = [3, 4, 5, 6]
+intersection = [num for num in list_1 if num in list_2]
+
+# Using string slice to reverse a word and lower() to lower it
+words = ["Elie", "Tim", "Matt"]
+reversed_and_lowered = [word[::-1].lower() for word in words]
