6532f0d008a6ae92a6ffc26c00fbb831c73f66dd
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-09 23:44:45 +0530

    Added code

---
+++ tutorials/basics/data-structures/dictionaries/dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionaries are a data structure consisting of key -value pairs (same as objects / maps in javascript or java)
+cat = {"name": "blue", "age": 3.5, "isCute": True}
+
+print(cat)
+
+# Another way to create a dictionary
+
+dictionary_2 = dict(name="Abhinav", age="25", works="b4s")
+
+print(dictionary_2)
