52ad1ba3ed16acb503e8266167747358093b7c94
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-11 00:00:15 +0530

    Added an exercise with solution

--- tutorials/basics/data-structures/dictionaries/exercises/exercise_1.py
+++ tutorials/basics/data-structures/dictionaries/exercises/exercise_1.py
@@ -1,1 +1,9 @@
 user_info = {"name": "Abhinav", "age": "25", "sex": "Male"}
+
+donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5,
+                 stan=150.0, lisa=50.25, harrison=10.0)
+
+total_donations = 0
+# Use a loop to add together all the donations and store the resulting number in a variable called total_donations
+for v in donations.values():
+    total_donations += v
