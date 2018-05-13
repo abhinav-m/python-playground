25a3a47166998ef6f04e1f854ac5e55903ed12f6
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-13 22:43:14 +0530

    Added exercise on dictionary

--- tutorials/basics/data-structures/dictionaries/exercises/exercise_1.py
+++ tutorials/basics/data-structures/dictionaries/exercises/exercise_1.py
@@ -7,3 +7,41 @@ total_donations = 0
 # Use a loop to add together all the donations and store the resulting number in a variable called total_donations
 for v in donations.values():
     total_donations += v
+
+# This code picks a random food item:
+from random import choice  # DON'T CHANGE!
+food = choice(["cheese pizza", "quiche", "morning bun",
+               "gummy bear", "tea cake"])  # DON'T CHANGE!
+
+# DON'T CHANGE THIS DICTIONARY EITHER!
+bakery_stock = {
+    "almond croissant": 12,
+    "toffee cookie": 3,
+    "morning bun": 1,
+    "chocolate chunk cookie": 9,
+    "tea cake": 25
+}
+
+# YOUR CODE GOES BELOW:
+if food in bakery_stock:
+    print(f"{bakery_stock[food]} left")
+else:
+    print("We don't make that")
+
+# Another solution using get:
+
+quantity = bakery_stock.get(food)
+
+if quantity:
+    print(f"{quantity} left")
+else:
+    print("We don't make that")
+
+
+# DO NOT TOUCH game_properties!
+game_properties = ["current_score", "high_score", "number_of_lives", "items_in_inventory", "power_ups", "ammo",
+                   "enemies_on_screen", "enemy_kills", "enemy_kill_streaks", "minutes_played", "notications", "achievements"]
+
+# Use the game_properties list and dict.fromkeys() to generate a dictionary with all values set to 0.  Save the result to a variabled called initial_game_state
+initial_game_state = dict.fromkeys(game_properties, 0)
+# Another solution initial_game_state = {}.fromkeys(game_properties,0)
