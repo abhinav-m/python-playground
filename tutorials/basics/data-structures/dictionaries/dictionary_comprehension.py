e9c7401d07fa30c009fa875fc18cc1e514d9fd92
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-14 23:42:00 +0530

    Added dictionary comprehension exercise

--- tutorials/basics/data-structures/dictionaries/dictionary_comprehension.py
+++ tutorials/basics/data-structures/dictionaries/dictionary_comprehension.py
@@ -6,6 +6,15 @@ numbers = dict(one=1,two= 2,three=3)
 squared_numbers = {key:value ** 2 for key,value in numbers.items()}
 print(squared_numbers)
 
-# Making a dictionary using a list.
+# Making a dictionary using a list. (iterable collection)
 cubed_numbers = {num:num**3 for num in [2,3,4,5,6]}
 print(cubed_numbers)
+
+# An example using strings and loops
+
+str_1 = "abhinav"
+str_2 = "mishraa"
+
+# In this case the iterable object is the string, assigning key value pairs of both strings to each other.
+interleaved = {str_1[i]:str_2[i] for i in range(0,len(str_1))}
+print(interleaved)
