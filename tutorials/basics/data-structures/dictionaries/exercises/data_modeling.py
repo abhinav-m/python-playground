394437d946f4da000f6db5d0c2f4f8d48e849af0
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-14 23:30:51 +0530

    Added an example on dictionaries

--- tutorials/basics/data-structures/dictionaries/exercises/data_modeling.py
+++ tutorials/basics/data-structures/dictionaries/exercises/data_modeling.py
@@ -1,19 +1,22 @@
 # Sample playlist data model in python.
 playlist =  {
-    title:"My playlist",
-    author:"Abhinav Mishra"
-    songs: [{
-    name:"Turn it off",
-    artists:["Culture Abuse"],
-    album:"Peach",
-    date:"2017-10-31",
-    length:"3:37"
+    "title":"My playlist",
+    "author":"Abhinav Mishra",
+    "songs": [{
+    "name":"Turn it off",
+    "artists":["Culture Abuse"],
+    "album":"Peach",
+    "date":"2017-10-31",
+    "length":"3:37"
     },
     {
-    name:"song_2",
-    artists:["test","test2"],
-    album:"Peach",
-    date:"2017-10-31",
-    length:"3:37"
+    "name":"song_2",
+    "artists":["test","test2"],
+    "album":"Peach",
+    "date":"2017-10-31",
+    "length":"3:37"
     }]
 }
+
+for song in playlist['songs']:
+    print(song["name"])
