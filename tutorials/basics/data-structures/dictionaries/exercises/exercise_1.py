24496b607276a7d2f4bbb2477be32b8f41bef06f
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-15 23:32:56 +0530

    Added exercise solution on dictionary

--- tutorials/basics/data-structures/dictionaries/exercises/exercise_1.py
+++ tutorials/basics/data-structures/dictionaries/exercises/exercise_1.py
@@ -46,15 +46,37 @@ game_properties = ["current_score", "high_score", "number_of_lives", "items_in_i
 initial_game_state = dict.fromkeys(game_properties, 0)
 # Another solution initial_game_state = {}.fromkeys(game_properties,0)
 
-inventory = {'croissant': 19, 'bagel': 4, 'muffin': 8, 'cake': 1} #DON'T CHANGE THIS LINE!
+inventory = {'croissant': 19, 'bagel': 4,
+             'muffin': 8, 'cake': 1}  # DON'T CHANGE THIS LINE!
 
 # Make a copy of inventory and save it to a variable called stock_list USE A DICTIONARY METHOD
-stock_list  = inventory.copy()
+stock_list = inventory.copy()
 
 # add the value 18 to stock_list under the key "cookie"
 
-stock_list.update({"cookie":18})
+stock_list.update({"cookie": 18})
 
 # remove 'cake' from stock_list USE A DICTIONARY METHOD
 stock_list.pop('cake')
 
+
+list1 = ["CA", "NJ", "RI"]
+list2 = ["California", "New Jersey", "Rhode Island"]
+
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer = {list1[i]: list2[i] for i in range(0, len(list1))}
+print(answer)
+
+# Key value pairs
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+
+# Directly creating a dictionary with the person nested list
+answer_2 = dict(person)
+print(answer_2)
+
+# Iterating over the dictionary
+answer_3 = {k: v for k, v in person}
+
+answer_4 = {item[0]: item[1] for item in person}
+
+print(answer_3, answer_4)
