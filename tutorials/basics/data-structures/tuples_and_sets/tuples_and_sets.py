31210bc1085401781c3a9f97cd96abdffeb39f8f
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-16 23:27:29 +0530

    Added tuples example

--- tutorials/basics/data-structures/tuples_and_sets/tuples_and_sets.py
+++ tutorials/basics/data-structures/tuples_and_sets/tuples_and_sets.py
@@ -2,3 +2,31 @@
 
 numbers = (1, 2, 3, 4)
 print(numbers)
+
+# Checking if value is in tuple
+print(3 in numbers)
+
+# Tuples are faster than lists, makes code safer, are valid keys in dictionaries.
+# Example of a good usage of tuples
+
+months = ('January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December')
+
+print(months)
+
+# Can be accessed using indexes just like lists.
+print(months[0], months[-1])
+
+# Creating office location dictionary with tuples as keys
+# Note you can't use a list as a key.
+locations = {
+    (35.6895, 39.1597): "Tokyo office",
+    (31.6895, -39.1597): "New york office",
+    (42.6895, 11.1597): "San francisco office",
+
+}
+
+print(locations)
+
+# Some dictionary methods return tuples
+print(locations.items())
