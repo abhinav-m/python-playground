3c02d5dea651dadb30f1ac49692269aed4ce6308
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-17 23:44:27 +0530

    Added tuple slice example

--- tutorials/basics/data-structures/tuples_and_sets/tuples_and_sets.py
+++ tutorials/basics/data-structures/tuples_and_sets/tuples_and_sets.py
@@ -50,3 +50,10 @@ print(tuple_example.count("B"))
 
 # returns the index of a tuple value
 print(names.index("Dhruv"))
+
+nested_tuple = (1,2,3,(4,5),6)
+print(nested_tuple)
+
+# tuples can be sliced using the same syntax shown earlier
+print(nested_tuple[0:])
+print(nested_tuple[1:4:2])
