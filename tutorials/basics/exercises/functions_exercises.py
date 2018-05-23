00a33c23634acf1c18749c21dd9d6808425ebdfe
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-05-23 23:54:30 +0530

    Added method examples

--- tutorials/basics/exercises/functions_exercises.py
+++ tutorials/basics/exercises/functions_exercises.py
@@ -26,3 +26,36 @@ def yell(what):
 
 
 print(yell("ayy"))
+
+# Define speak below:
+
+
+def speak(animal="dog"):
+    if(animal == "dog"):
+        return "woof"
+    elif(animal == "pig"):
+        return "oink"
+    elif(animal == "duck"):
+        return "quack"
+    elif(animal == "cat"):
+        return "meow"
+    elif(animal == "banana"):
+        return "?"
+
+
+print(speak())
+print(speak("cat"))
+print(speak("banana"))
+
+
+def speak_dict(animal='dog'):
+    noises = {'pig': 'oink', 'duck': 'quack', 'cat': 'meow', 'dog': 'woof'}
+    return noises.get(animal, '?')
+
+
+def speak_dict_2(animal="dog"):
+    noises = {"dog": "woof", "pig": "oink", "duck": "quack", "cat": "meow"}
+    noise = noises.get(animal)
+    if noise:
+        return noise
+    return "?"
