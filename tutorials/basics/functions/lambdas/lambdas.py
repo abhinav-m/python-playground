3065413c91bf81253e875e7e3f78f33ac347a417
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-06-14 23:07:49 +0530

    Added example for lambda functions

---
+++ tutorials/basics/functions/lambdas/lambdas.py
@@ -0,0 +1,6 @@
+# Lambda functions are similar to anonymous functions in other languages. (eg Javascript)
+def square(num): return num*num
+
+# Syntax for lambda functions, returns square of number
+square2 = lambda num: num * num
+print(square2(9))
