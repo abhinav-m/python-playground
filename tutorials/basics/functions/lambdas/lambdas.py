c0d4f75ef36d122109d4c35e746bc61aa3471e19
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-06-14 23:33:07 +0530

    Added code for doubles

--- tutorials/basics/functions/lambdas/lambdas.py
+++ tutorials/basics/functions/lambdas/lambdas.py
@@ -2,5 +2,24 @@
 def square(num): return num*num
 
 # Syntax for lambda functions, returns square of number
+# Automatically returns the expression after colon.
 square2 = lambda num: num * num
 print(square2(9))
+
+# Lambda functions are used when passing functions as values to other functions.(generally)
+# Write a lambda that accepts a single number and cubes it. Save it in a variable called cube.
+
+cube = lambda num: num * num * num
+
+##  Lambda function cubed.
+cubed = lambda number: number ** 3
+
+# Usage of lambda functions in higher order function such as map
+def double(x) : return x*2
+
+double(3)
+
+test_list = [1,2,3,4,5]
+doubles = list(map(lambda x:x*2,test_list))
+
+print(doubles)
